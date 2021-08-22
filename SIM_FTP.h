711312b127c289d356a1e72dd019ec5f3df9702c
cjsaiz89 <89260258+cjsaiz89@users.noreply.github.com>
2021-08-22 14:30:29 -0400

    Update SIM_FTP.h

--- SIM_FTP.h
+++ SIM_FTP.h
@@ -17,21 +17,12 @@
 #define SECRET_GPRS_LOGIN    "" // replace with your GPRS login
 #define SECRET_GPRS_PASSWORD "" // replace with your GPRS password
 
-
-
 // FTP info1
-#define SECRET_FTP_HOST_2             "f19-preview.royalwebhosting.net"  // replace with your FTP host server
-#define SECRET_FTP_USER_2            "3391543"  // replace with your FTP user
-#define SECRET_FTP_PASSWORD_2         "123q789q456q"  // replace with your FTP password
+#define SECRET_FTP_HOST_2             "ftp"  // replace with your FTP host server
+#define SECRET_FTP_USER_2            "usr"  // replace with your FTP user
+#define SECRET_FTP_PASSWORD_2         "pass"  // replace with your FTP password
 #define SECRET_FTP_PORT_2             21  // replace with your FTP port (optional)
-#define SECRET_FTP_REMOTE_DIR_2       "/Radiometer" // replace with your FTP default remote directory
-
-// FTP info2 - IMPORTANT: change 'uint32_t c_connectionTimeout' to 30000 in GSMFTP.h file. Increase other timeouts
-#define SECRET_FTP_HOST_1             "ftp.aoml.noaa.gov"  // replace with your FTP host server
-#define SECRET_FTP_USER_1             "christian.saiz"  // replace with your FTP user
-#define SECRET_FTP_PASSWORD_1         "Kircchoff.072019"  // replace with your FTP password
-#define SECRET_FTP_PORT_1             21  // replace with your FTP port (optional)
-#define SECRET_FTP_REMOTE_DIR_1       "/phod/incoming/christian.saiz/radiometer01" // replace with your FTP default remote directory
+#define SECRET_FTP_REMOTE_DIR_2       "/dir" // replace with your FTP default remote directory
 
 
 // Assign SIM card info
